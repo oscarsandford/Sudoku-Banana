1676ed1cc7be733d03d18c0c4c3c4b4d12cdbe98
Oscar Sandford <oscarsandford@users.noreply.github.com>
2020-04-23 16:19:06 -0700

    finished this bs

--- SudokuCube.java
+++ SudokuCube.java
@@ -2,13 +2,15 @@ public class SudokuCube {
 
   public int[][] cube;
   public int dim;
-  public int dim_rt;
-  private int hint_rarity_factor = 1;   // ideally 2
+  private int dim_rt;
+  public boolean possible = true;
+  private int space_factor;   // ideally 2
 
-  public SudokuCube(int n) {
+  public SudokuCube(int n, int s) {
     dim = n;
     dim_rt = (int)Math.sqrt(dim);
     cube = new int[dim][dim];
+    space_factor = s;
     create_sudoku();
   }
 
@@ -57,27 +59,11 @@ public class SudokuCube {
 
   // return a good number for ij-index, 0's are blank spaces
   private int create_slot(int i, int j) {
-    int num = random_hint(0, hint_rarity_factor*dim);
+    int num = random_hint(0, space_factor*dim);
     // refresh if num invalid, good if 0
     while (invalid_num(i, j, num) && num != 0) {
-      num = random_hint(0, hint_rarity_factor*dim);
+      num = random_hint(0, space_factor*dim);
     }
-    return num;
-  }
-
-  // solves the number for blank spaces
-  private int solve_slot(int i, int j) {
-    int num = random_hint(1, dim);
-    System.out.println("Solving slot [" + i + "," + j + "] . . .");
-    // !!! this won't work grr
-    /* It can't find a good num when there's a bunch of different options
-      It just gets stuck ..
-      I think we need a separate method to help invalid_num */
-
-    while(invalid_num(i, j, num)) {
-      num = random_hint(1, dim);
-    }
-
     return num;
   }
 
@@ -90,36 +76,83 @@ public class SudokuCube {
     }
   }
 
-  // solve the puzzle
-  public void solve_sudoku() {
+  /* I got this function from the internet because I was frustrated,
+    but it turns out my problem was not generating enough puzzles
+    because my hint generation makes some of them impossible. Yikes.*/
+  public boolean solve_sudoku() {
+    int r = -1;
+    int c = -1;
+    boolean is_empty = true;
+
+    // this is shit to do every time
     for (int i = 0; i < dim; i++) {
       for (int j = 0; j < dim; j++) {
         if (cube[i][j] == 0) {
-          cube[i][j] = solve_slot(i, j);
+          r = i;
+          c = j;
+          is_empty = false;
+          break;
+        }
+      }
+      if (!is_empty) {
+        break;
+      }
+    }
+    // we done baby let's go
+    if (is_empty) {
+      return true;
+    }
+
+    for (int num = 1; num <= dim; num++) {
+      if (!invalid_num(r, c, num)) {
+        cube[r][c] = num;
+        // if possible to solve this, return true
+        if (solve_sudoku()) {
+          return true;
+        }
+        // else make this slot 0 and try again later
+        else {
+          cube[r][c] = 0;
         }
       }
     }
+    // holy shoot kill me
+    return false;
   }
 
-  // returns the 2D puzzle board in its current state
+  /* solver function helper so I can try out some other methods
+    of solving the problem using backtracking */
+  public void solve() {
+    if (!solve_sudoku()) {
+      possible = false;
+    }
+  }
+
+  // returns the 2D puzzle board in its current state, awful syntax
   public String toString() {
-    String s = "-------------------------\n";
+    int max_num_len = (dim + "").length();
+    String line = new String(new char[ (dim_rt*(dim_rt+1))+(dim_rt+1)+(dim*max_num_len) ]).replace("\0", "-") + "\n";
+    String s = line;
+
     for (int i = 0; i < dim; i++) {
       for (int j = 0; j < dim; j++) {
+        int num_len = (cube[i][j] + "").length();
+        String offset = new String(new char[max_num_len-num_len]).replace("\0", " ");
+
         if (j % dim_rt == 0) {
           s += "| ";
         }
-        // print a black space if there's a 0
-        if (cube[j][i] == 0) {
-          s+= "  ";
+        // print a blank space if there's a 0
+        if (cube[i][j] == 0) {
+          s+= new String(new char[max_num_len+1]).replace("\0", " ");
         }
         else {
-          s += cube[j][i] + " ";
+          s += offset + cube[i][j] + " ";
         }
       }
       s += "|\n";
       if ((i+1) % dim_rt == 0)
-         s += "-------------------------\n";
+         s += line;
     }
     return s;
   }
--- SudokuSolver.java
+++ SudokuSolver.java
@@ -1,13 +1,59 @@
 class SudokuSolver {
 
+  public static void multiple_solver(int d, int h, int m) {
+    int solved = 0;
+
+    for (int i = 0; i < m; i++) {
+      SudokuCube puzzle = new SudokuCube(d, h);
+      System.out.println(". . . Processing problem #"+(i+1));
+      puzzle.solve();
+      if (puzzle.possible) {
+        System.out.println(puzzle.toString());
+        solved++;
+      }
+    }
+    double percent = (double)solved / (double)m;
+    String ans = "\n\tDimension: " + d;
+    ans += "\n\tSpace Factor: "+h+"\n\tTotal: "+m;
+    ans += "\n\tSolved: "+solved+" ("+(percent*100)+"%)";
+    System.out.println(ans);
+  }
+
+
   public static void main(String[] args) {
 
     int dimension = 9;
-    SudokuCube puzzle = new SudokuCube(dimension);
+    int space_factor = 2;
+    int multiples = 1;
 
-    System.out.println("Puzzle:\n" + puzzle.toString());
-    puzzle.solve_sudoku();
-    System.out.println("Solved:\n" + puzzle.toString());
+    // whole bunch of optional input stuff - defaults are above
+    // arg1: board dimension (must be a perfect square, default 9)
+    // arg2: number of puzzles to generate
+    // arg3: factor for amount of blank spaces on starter board
+    try {
+      dimension = Integer.parseInt(args[0]);
+      int rt = (int)Math.sqrt(dimension);
+      if (rt*rt != dimension) {
+       throw new Exception();
+      }
+      try {
+        multiples = Integer.parseInt(args[1]);
+      } catch (Exception e) {
+        multiples = 1;
+      }
+      try {
+        space_factor = Integer.parseInt(args[2]);
+      } catch (Exception e) {
+        space_factor = 2;
+      }
+    }
+    catch (Exception e) {
+      dimension = 9;
+    }
 
-  }
+    // solve all the shit
+    multiple_solver(dimension, space_factor, multiples);
+
+
+  } // end of main
 }
